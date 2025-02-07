35dbe9b18a0a48380bbe9379b822785241590b35
Rene Hart <email.com>
2025-02-07 12:29:27 -0500

    Print Tras Date

---
+++ Ad.py
@@ -0,0 +1,9 @@
+#Declare Variables
+from datetime import date
+CustID = 0
+Store_string= "ABCD"
+TrasID = 0
+TrasDate = date(2024,1,31)
+Amount = 0 #Must be two decimals, still needs work
+
+print(TrasDate)
